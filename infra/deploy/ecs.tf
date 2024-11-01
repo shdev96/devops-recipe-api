9c4c98f49d3a551d4fde5ebef7181793d3981ac0
shdev96 <seunghee.dev@gmail.com>
2024-11-01 22:53:44 +0900

    fix

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -203,3 +203,7 @@ resource "aws_ecs_service" "api" {
     security_groups = [aws_security_group.ecs_service.id]
   }
 }
+
+resource "aws_iam_service_linked_role" "ecs" {
+  aws_service_name = "ecs.amazonaws.com"
+}
