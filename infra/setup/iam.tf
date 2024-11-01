0fdc757b6504f6b29700ab357b383ceff755fd42
shdev96 <seunghee.dev@gmail.com>
2024-11-01 22:37:57 +0900

    fix

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -245,7 +245,9 @@ data "aws_iam_policy_document" "iam" {
       "iam:TagRole",
       "iam:TagPolicy",
       "iam:PassRole",
-      "iam:CreateServiceLinkedRole"
+      "iam:CreateServiceLinkedRole",
+      "iam:DeleteServiceLinkedRole",
+      "iam:GetServiceLinkedRoleDeletionStatus"
     ]
     resources = ["*"]
   }
