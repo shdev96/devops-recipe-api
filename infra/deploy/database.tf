130ae46abff9b1cc9b8e8c92ce712a57aa3499f8
shdev96 <seunghee.dev@gmail.com>
2024-11-01 15:14:46 +0900

    add ecs service

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -23,6 +23,8 @@ resource "aws_security_group" "rds" {
     protocol  = "tcp"
     from_port = 5432
     to_port   = 5432
+
+    # can access containers that have below security group
     security_groups = [
       aws_security_group.ecs_service.id
     ]
