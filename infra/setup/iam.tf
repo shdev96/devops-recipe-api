0fc539376999b4e075ce9c970fd719f0762a4a1b
shdev96 <seunghee.dev@gmail.com>
2024-11-02 01:44:07 +0900

    add route53

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -135,7 +135,10 @@ data "aws_iam_policy_document" "ec2" {
       "ec2:ModifyVpcAttribute",
       "ec2:RevokeSecurityGroupIngress",
       "ec2:DescribeAvailabilityZones",
-      "ec2:DescribeAccountAttributes"
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeNetworkInterfaceAttribute",
+      "ec2:CreateNetworkInterface",
+      "ec2:DeleteNetworkInterface"
     ]
     resources = ["*"]
   }
@@ -370,4 +373,42 @@ resource "aws_iam_policy" "efs" {
 resource "aws_iam_user_policy_attachment" "efs" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.efs.arn
+}
+
+#############################
+# Policy for Route53 access #
+#############################
+
+data "aws_iam_policy_document" "route53" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListHostedZones",
+      "route53:ChangeTagsForResource",
+      "route53:GetHostedZone",
+      "route53:ListTagsForResource",
+      "route53:ChangeResourceRecordSets",
+      "route53:GetChange",
+      "route53:ListResourceRecordSets",
+      "acm:RequestCertificate",
+      "acm:AddTagsToCertificate",
+      "acm:DescribeCertificate",
+      "acm:ListTagsForCertificate",
+      "acm:DeleteCertificate",
+      "acm:CreateCertificate"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "route53" {
+  name        = "${aws_iam_user.cd.name}-route53"
+  description = "Allow user to manage Route53 resources."
+  policy      = data.aws_iam_policy_document.route53.json
+}
+
+resource "aws_iam_user_policy_attachment" "route53" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.route53.arn
 }
